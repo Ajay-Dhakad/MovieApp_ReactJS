9d02bd4080dec911a16e4e8a94fccb3aefc2a718
Ajay-Dhakad <ajaydhakad.py@gmail.com>
2024-01-19 02:58:20 +0530

    framer motion integrated in cards !

--- src/Components/MovieCards.jsx
+++ src/Components/MovieCards.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import {motion} from 'framer-motion'
 
 function movieCards({data }) {
   console.log(data)
   return (
    <>
    
-   <div className="movieCard">
+   <motion.div className="movieCard"
+   
+   initial={{opacity:0}}
+   whileInView={{opacity:1,threshold: 0.9}}
+   transition={{duration:1,delay:.1}}
+
+   viewport={{once:true}}
+   
+   >
     
     <div className="movieimage">
       <img src={data.Poster != 'N/A' ? data.Poster : 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/832px-No-Image-Placeholder.svg.png'} alt="" />
@@ -15,7 +24,7 @@ function movieCards({data }) {
       <p>Release Year : {data.Year}</p>
       </div>
    
-   </div>
+   </motion.div>
   
 
    </>
