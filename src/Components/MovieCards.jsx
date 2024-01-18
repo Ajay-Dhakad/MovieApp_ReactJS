63915e0fdff3e5672e0f1b155013e09268e808f6
Ajay-Dhakad <ajaydhakad.py@gmail.com>
2024-01-18 13:27:01 +0530

    cards updated and styles added !

--- src/Components/MovieCards.jsx
+++ src/Components/MovieCards.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
-function movieCards({data}) {
+function movieCards({data }) {
   console.log(data)
   return (
    <>
    
    <div className="movieCard">
-    <h1 key={data.imdbID}>{data.Title}</h1>
+    
+    <div className="movieimage">
+      <img src={data.Poster} alt="" />
+      </div>
+   
    </div>
   
 
--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -19,6 +19,8 @@ function SearchResults({movie}) {
 
     {!movieData && <>loading...</>}
 
+    
+
 
     </div>
   )
