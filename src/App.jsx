66931f9078d6c02cd5f01d1c25fc2ae9332234cb
Ajay-Dhakad <ajaydhakad.py@gmail.com>
2024-01-25 02:59:22 +0530

    routes sroll to top updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import { useState } from 'react'
-import {Outlet} from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import {Outlet,useLocation} from 'react-router-dom'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 function App() {
+
+  const {pathname} = useLocation()
+  
+  useEffect(() => {
+
+    window.scrollTo({top:0,behavior:'smooth'})
+
+  },[pathname])
   
   return (
     
