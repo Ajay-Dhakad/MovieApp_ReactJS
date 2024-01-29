2aa5da39d4d0c181753bbd50cbab0b6df4c8f80b
Ajay-Dhakad <ajaydhakad.py@gmail.com>
2024-01-30 00:09:55 +0530

    footer text added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 function Footer() {
   return (
    <footer>
-
+    <div className="devsocials">
+   <img src="" alt="" /><img src="" alt="" /><img src="" alt="" />
+    </div>
+<p>©2022 Cinescore,All Rights Reserved </p>
     
    </footer>
   )
