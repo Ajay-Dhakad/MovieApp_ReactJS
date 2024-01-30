4d957cafeb241dcb1bd3b514250e7d309b3de9b8
Ajay-Dhakad <ajaydhakad.py@gmail.com>
2024-01-31 03:22:02 +0530

    updated and 404 page added !

--- src/Components/UrlNotFound.jsx
+++ src/Components/UrlNotFound.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {motion} from 'framer-motion'
 
 function UrlNotFound() {
   return (
     <div className='movie-results'>
 
-<div className="page404">
-    <h1>404</h1>
+<motion.div drag dragConstraints={{left: 0,right: 0,top: 0,bottom: 0,}} initial={{opacity:0}} transition={{duration:2}} animate={{opacity:1}} className="page404">
+    <h1 >404</h1>
     <p>Page Not Found</p>
     <br />
     <Link to={'/'}>Go Home</Link>
     
-</div>
+</motion.div>
         
       
     </div>
